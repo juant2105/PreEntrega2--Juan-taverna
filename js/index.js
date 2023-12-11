633d94077c89b1c64214fc4319de8b0d37bcc489
Juan Taverna <juantaverna2105@gmail.com>
2023-12-10 22:10:34 -0300

    navbar tipo hamburguesa agregada

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".hamburguesa")
+const navToggle = document.querySelector(".navmenu")
+
+navToggle.addEventListener("click" , () => {
+   navMenu.classList.toggle("navmenu_visible") 
+})
